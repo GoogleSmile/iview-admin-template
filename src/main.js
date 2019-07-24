ee073917e823d63d274f3f779185eb55f776dd78
GoogleSmileMr <GoogleSmileMr@github.com>
2019-07-24 23:59:03 +0800

    login登录页面

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import iView from 'iview'
+import config from '@/config'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(iView)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$config = config
 
 new Vue({
   router,
