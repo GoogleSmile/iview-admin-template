37d2645f0715957aaaf883ad9c15cadf6785dc58
yangchun.ma <yangchun.ma@gaeamobile.com>
2019-08-11 10:57:30 +0800

    home页

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import iView from 'iview'
 import config from '@/config'
+import i18n from '@/locale'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(iView)
@@ -15,5 +16,6 @@ Vue.prototype.$config = config
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
